f21ba12cc42efcb65db0b38b2b165b4c9ffaabbb
Ahmad raza <119778503+Ahmad5626@users.noreply.github.com>
2025-04-01 16:26:20 +0530

    login page create

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -483,3 +483,15 @@ jQuery(document).ready(function($) {
         });
       }
 	});
+        
+
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/67a75199825083258e122a84/1ijipdg1v';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    
